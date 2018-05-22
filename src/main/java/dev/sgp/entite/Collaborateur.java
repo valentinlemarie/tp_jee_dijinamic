26307d471e86406feb79cfb782fec872f31c7400
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-05-22 17:29:48 +0200

    tp3 - Service Technique Collaborateurs

---
+++ src/main/java/dev/sgp/entite/Collaborateur.java
@@ -0,0 +1,18 @@
+package dev.sgp.entite;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+
+public class Collaborateur {
+	String matricule;
+	String nom ;
+	String prenom;
+	LocalDate dateDeNaissance;
+	String Adresse;
+	String NumeroDeSecuriteSociale; 
+	String emailPro;
+	String photo ;
+	ZonedDateTime dateHeureCreation;
+	Boolean actif;
+}
